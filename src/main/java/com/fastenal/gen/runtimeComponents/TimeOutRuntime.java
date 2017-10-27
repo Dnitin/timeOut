94589b8f9317678e7b5ef4f09135471456e1f8d1
Nitin Ranjan <n.kr@outlook.in>
2017-10-27 20:22:44 +0530

    implemented remaining time calculator

--- src/main/java/com/fastenal/gen/runtimeComponents/TimeOutRuntime.java
+++ src/main/java/com/fastenal/gen/runtimeComponents/TimeOutRuntime.java
@@ -12,6 +12,7 @@ import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
@@ -32,7 +33,7 @@ public class TimeOutRuntime {
     @Autowired
     private ObjectMapper objectMapper;
 
-    public Map<String, Map<Date, String>> obtainWeekRecord() {
+    public Map<String, String> obtainWeekRecord() {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         HttpEntity<Object> entity = null;
@@ -43,9 +44,7 @@ public class TimeOutRuntime {
         }
         ResponseEntity<String> postResponse = restTemplate.exchange(requestLeave.getUrl(), HttpMethod.POST, entity, String.class);
         Map<String, String> preContainer;
-        Map<String, Map<String, Map<Date, String>>> weekRecords = new HashMap<>();
-        DateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-        objectMapper.setDateFormat(sdf);
+        Map<String, Map<String, String>> weekRecords = new HashMap<>();
 
         try {
             preContainer = objectMapper.readValue(postResponse.getBody(), new TypeReference<HashMap>() {
@@ -54,12 +53,13 @@ public class TimeOutRuntime {
             });
         } catch (IOException e) {
             e.printStackTrace();
+            System.out.println("just");
             return null;
         }
         return weekRecords.get("WeeklyHours");
     }
 
-    public Map<Integer, Map<Date, String>> obtainSwipeRecord() {
+    public Map<Integer, Map<String, String>> obtainSwipeRecord() {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         HttpEntity<Object> entity = null;
@@ -70,9 +70,7 @@ public class TimeOutRuntime {
         }
         ResponseEntity<String> postResponse = restTemplate.exchange(requestSwipe.getUrl(), HttpMethod.POST, entity, String.class);
         Map<String, String> garbageContainer;
-        Map<String, Map<Integer, Map<Date, String>>> swipeRecords = new HashMap<>();
-        DateFormat sdf = new SimpleDateFormat("hh:mm aaa");
-        objectMapper.setDateFormat(sdf);
+        Map<String, Map<Integer, Map<String, String>>> swipeRecords = new HashMap<>();
 
         try {
             garbageContainer = objectMapper.readValue(postResponse.getBody(), new TypeReference<HashMap>() {
@@ -84,5 +82,55 @@ public class TimeOutRuntime {
             return null;
         }
         return swipeRecords.get("SwipeRecord");
+    }
+
+    public long calculateWeeklyRemainingTime() {
+        Map<String, String> weeklyhours = obtainWeekRecord();
+        Double hours = null;
+        for (String key : weeklyhours.keySet()) {
+            String weekTotal = weeklyhours.get(key).split(";")[1];
+            hours = Double.valueOf(weekTotal);
+        }
+        //int days = hours%8;
+        Double inMillis = hours * 3600000;
+        return 0;
+    }
+
+    public long calculateRemainingMillis() {
+        Map<Integer, Map<String, String>> swipeRecord = obtainSwipeRecord();
+        long millis = 0;
+        long totalMillis = 28800000;
+        String prevVal = "darbage";
+        String currVal = "garbage";
+        Date currDate = new Date();
+        Date prevDate = new Date();
+        SimpleDateFormat formatter = new SimpleDateFormat("hh:mm a");
+        for (Map.Entry<Integer, Map<String, String>> entry : swipeRecord.entrySet()) {
+            currVal = entry.getValue().get("swipeInOut");
+            try {
+                currDate = formatter.parse(entry.getValue().get("swipeTime"));
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            if (prevVal.equalsIgnoreCase("In") && currVal.equalsIgnoreCase("Out")) {
+                millis += currDate.getTime() - prevDate.getTime();
+                prevVal = "garbage";
+                currVal = "garbage";
+            } else {
+                prevVal = currVal;
+                prevDate = currDate;
+            }
+        }
+
+        if (prevVal.equalsIgnoreCase("In")) {
+            String todayDate = formatter.format(new Date());
+            try {
+                currDate = formatter.parse(todayDate);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            millis += currDate.getTime() - prevDate.getTime();
+        }
+        return totalMillis - millis;
     }
 }
